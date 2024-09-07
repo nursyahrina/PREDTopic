e327f1ef3b2140e993dae0efc3973031d79aa961
nursyahrina <nursyahrina17@gmail.com>
2024-09-07 16:20:42 +0700

    fix nltk download

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -11,6 +11,7 @@ nltk.download("punkt_tab")
 nltk.download("stopwords")
 nltk.download("wordnet")
 nltk.download("averaged_perceptron_tagger")
+nltk.download("averaged_perceptron_tagger_eng")
 
 
 # Define preprocessing functions
